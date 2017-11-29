d8731da930346aa5ca7e402b6c6710a2a7c7d9c7
unknown <v.a.stekolschikov@gmail.com>
2017-11-29 12:50:45 +0200

    start

---
+++ 9/src/Header.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <input type="text" :value="mess" @input="onInput">
+    </div>
+ 
+</template>
+
+<script>
+export default {
+  props: ["mess"],
+  methods: {
+    onInput(event) {
+      this.$root.$emit("mess-send", event.target.value);
+    }
+  },
+
+};
+</script>
